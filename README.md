26a701da9dcb913ec2529ae7d0b7dfcdd9827ff4
kawgh1 <krseven@gmail.com>
2023-08-21 22:33:39 -0500

    add MongoDb user authentication with .NET Secret Manager and added Health Checks to our API using nuget package AspNetCore.HealthChecks.MongoDb

--- README.md
+++ README.md
@@ -36,3 +36,66 @@
   <br>
 - View running Docker processes
   - `docker ps`
+- Stop running Docker process
+  - `docker stop {name}`
+- View Docker Volumes
+  - `docker volume ls`
+- Remove Docker Volume
+  - `docker volume rm {volume name}`
+
+  <br>
+
+- Run local Docker Container with environment variables (DB username, password, etc.)
+  - `docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=password1 mongo`
+    - `-d` - dont attach to the process, just run it and let it go
+    - `-rm` - destroy the container after you are done, dont keep it running
+    - `--name` - name of the Image / Container you are creating
+    - `-p` - the port
+    - `-v` - the MongoDB volume, so you dont destroy the data when you stop the Docker Container
+    - `mongodbdata:/data/db mongo` - store your DB data at this file path in Mongo to persist the data after the Container has closed
+      - So at this point, our DB knows about requiring user authentication we have set, but our Services do not know about it
+        - Set these in `appsettings.json` and the `.NET Secret Manager`
+
+- `.NET Secret Manager` - Generally dont use this in Prod
+  - `dotnet user-secrets init`
+    - `Set UserSecretsId to '7ee7c261-9b12-4f9e-9fc0-de16f4fd7384' for MSBuild project '/Users/j/Desktop/Catalog/Catalog.csproj'.`
+  - to add a secret
+    - `dotnet user-secrets set MongoDbSettings:Password password1`
+      - `Successfully saved MongoDbSettings:Password = password1 to the secret store.`
+  - Finally
+    - Set the username and password in the MongoDB Connection string in `settings/mongodbsettings.cs`
+      - `return $"mongodb://{User}:{Password}@{Host}:{Port}";`
+
+  <br>
+
+- ### Health Checks
+  - A health check is an endpoint like `https://localhost:5001/health` that will tell if an API is able to receive requests and able to communicate with the DB or other services it depends on
+  - Install Open Source MongoDB library to check MongoDB connection status as part of Health Check
+    - `dotnet add package AspNetCore.HealthChecks.MongoDb`
+  - This can be configured in `startup.cs`
+    - `services.AddHealthChecks().AddMongoDb(mongoDbSettings.ConnectionString, name: "mongodb", timeout: TimeSpan.FromSeconds(3));`
+    - `app.UseEndpoints(endpoints =>
+      {
+      endpoints.MapControllers();
+      endpoints.MapHealthChecks("/health");
+      });`
+  - Testing Health Check can be done by sending a GET request to https://localhost:5001/health 
+    - it should return `200` `Healthy`
+    - Now stop the docker instance of the Mongo container `docker stop mongo`
+    - Send another GET request to https://localhost:5001/health
+    - Verify it returns `503` `Unhealthy`
+  - #### Note: I had to add another package to get the MongoDB Health Check package to work properly
+    - `dotnet add package Microsoft.Bcl.AsyncInterfaces`
+  - Health Checks can be customized and configured to return more detailed information as a JSON object like this:
+    - `{
+        "status": "Healthy",
+            "checks": [
+                {
+                "name": "mongodb",
+                "status": "Healthy",
+                "exception": "none",
+                "duration": "00:00:00.0923151"
+                }
+            ]
+      }`
+    - More customization options can be seen here https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks for all kinds or BackEnd stuff including AWS, Kafka, RabbitMQ, SendGrid, Kubernetes, Azure, etc.
--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Catalog.Repositories;
 using Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -36,12 +40,14 @@ namespace Catalog
             // tells Mongo any time you see a Guid or DateTimeOffset, it should serialize it as a string
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            // MongoDb Settings
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
+
             
             // start Mongo Client instance
             services.AddSingleton<IMongoClient>(serviceProvider =>
             {
-                var settings = Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDBSettings>();
-                return new MongoClient(settings.ConnectionString);
+                return new MongoClient(mongoDbSettings.ConnectionString);
             });
             
             // services.AddSingleton<IItemsRepository, InMemoryItemsRepository>(); // register dependencies
@@ -55,6 +61,13 @@ namespace Catalog
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
             });
+            
+            // Add Health Checks Service
+            services.AddHealthChecks().AddMongoDb(
+                mongoDbSettings.ConnectionString, 
+                name: "mongodb", 
+                timeout: TimeSpan.FromSeconds(3),
+                tags: new[] {"ready"});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -76,6 +89,36 @@ namespace Catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                // 'ready' means our service is connected to its DB and ready to receive requests
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+                {
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async (context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new
+                            {
+                                status = report.Status.ToString(),
+                                checks = report.Entries.Select(entry => new
+                                    {
+                                        name = entry.Key,
+                                        status = entry.Value.Status.ToString(),
+                                        exception = entry.Value.Exception != null
+                                            ? entry.Value.Exception.Message
+                                            : "none",
+                                        duration = entry.Value.Duration.ToString()
+                                    }
+                                )
+                            });
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+                // 'live' means our service is running but no further info, skip any further checks
+                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+                {
+                    Predicate = (_) => false
+                });
             });
         }
     }
